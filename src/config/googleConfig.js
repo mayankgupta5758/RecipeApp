620a5425b58ccf3cdb071a43fad272c18c100d2b
Mayank Gupta <125710019+mayankgupta5758@users.noreply.github.com>
2026-07-20 22:47:39 +0530

    Update webClientId in googleConfig.js

--- src/config/googleConfig.js
+++ src/config/googleConfig.js
@@ -1,6 +1,6 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 GoogleSignin.configure({
-    webClientId: "773546142873-k4v9emsj4n9ebkptdtj6g55lmqhka4pd.apps.googleusercontent.com",
+    webClientId: "WEBClientId",
      offlineAccess:true
 })
